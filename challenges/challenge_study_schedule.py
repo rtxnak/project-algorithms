31835daa8951f5de264ad3bf34af22bfd5c510b0
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-08-30 19:48:07 -0300

    refact: refatoracao para remover try e except

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,9 +1,10 @@
 def study_schedule(permanence_period, target_time):
     counter = 0
-    try:
-        for first, second in permanence_period:
-            if first <= target_time and second >= target_time:
-                counter += 1
-        return counter
-    except:
-        return None
+    for first, second in permanence_period:
+        if not isinstance(first, int) or not isinstance(second, int):
+            return None
+        if not isinstance(target_time, int):
+            return None
+        if first <= target_time and second >= target_time:
+            counter += 1
+    return counter
