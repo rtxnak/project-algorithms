76d639f44ea7364ff41dad1db8b982299dbea23b
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-08-30 16:40:56 -0300

    feat: adicionou funcao study_schedule - requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    counter = 0
+    try:
+        for first, second in permanence_period:
+            if first <= target_time and second >= target_time:
+                counter += 1
+        return counter
+    except:
+        return None
